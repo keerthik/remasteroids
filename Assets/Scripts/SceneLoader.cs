145fa9751da41c4298dc7348a5897344d7e88af0
keerthik <keerthiko@gmail.com>
2023-02-03 05:33:00 -0500

    real initial commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Loads a file from disk for seeding this scene
+    // Writes a file to disk when saving this scene
+
+    class SceneData {
+        string sceneName;
+        string gameVersion;
+    }
+
+    private SceneData data;
+    void Start()
+    {  
+        Debug.Log($"Scene loader started at time {Time.time}");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
