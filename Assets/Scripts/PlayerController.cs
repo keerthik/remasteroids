bf562b83fed4aa3cf39f6a590137c2d5d01d5268
keerthik <keerthiko@gmail.com>
2023-02-11 05:58:58 -0500

    playercontroller stub

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    const float maxSpeed = 81f;
+    Vector2 speed = new Vector2(0, 0);
+    void Move(Vector2 direction) {
+
+    }
+
+    void Update() {
+    }    
+}
